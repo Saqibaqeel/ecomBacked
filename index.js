91471741c123f30945853e3ba80c5aa0619c9750
Saqibaqeel <Saquibaqeel@gmail.com>
2025-11-23 20:04:01 +0530

    added cors

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ app.use(express.json());
 
 // Routes
 app.use('/api', clothRoutes);
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173"
+}));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
