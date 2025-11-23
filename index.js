c94fb0d15696558658fb733827877a16c2b692da
Saqibaqeel <Saquibaqeel@gmail.com>
2025-11-23 21:06:26 +0530

    fix cors

--- index.js
+++ index.js
@@ -4,22 +4,23 @@ const cors = require('cors');
 const connectDb = require('./connectDb');
 const clothRoutes = require('./routes/clothRoutes');
 
-
 dotenv.config();
 connectDb();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Enable CORS for everyone
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
 app.use(express.json());
 
 // Routes
 app.use('/api', clothRoutes);
-app.use(cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-}));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
